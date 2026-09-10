d907b7719b8a6b88d3f07979cbeb2db315e336da
Yuchan Lee <lasuillard@gmail.com>
2026-09-10 14:01:30 +0000

    Output `psql` command for use

--- infrastructure/setup/outputs.tf
+++ infrastructure/setup/outputs.tf
@@ -2,3 +2,14 @@ output "base_snapshot_id" {
   description = "Base snapshot identifier to use for rotation."
   value       = local.db_snapshot_identifier
 }
+
+output "psql_command" {
+  description = "`psql` command to connect to the database."
+  value       = <<-CMD
+    PGUSER='${local.db_username}' \
+    PGPASSWORD='${local.db_password}' \
+    PGDATABASE='${local.db_name}' \
+    ${abspath("${path.module}/../../scripts/psql.sh")} \
+      5432 '${aws_instance.bastion.id}' '${local.db_host}' '${local.db_port}'
+  CMD
+}
