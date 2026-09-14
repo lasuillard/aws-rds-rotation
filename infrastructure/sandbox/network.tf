8ad5d4d2f9b3cc7598f70ef41742acc0664343b7
Yuchan Lee <lasuillard@gmail.com>
2026-09-14 05:15:41 +0000

    Fix missing route to NAT for private subnets

--- infrastructure/sandbox/network.tf
+++ infrastructure/sandbox/network.tf
@@ -18,6 +18,24 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
 # At least 2 subnets in different AZs are required for RDS, even though only 1 will be used
 resource "aws_subnet" "private_1" {
   vpc_id            = aws_vpc.main.id
@@ -37,6 +55,29 @@ resource "aws_subnet" "private_2" {
   tags = {
     Name = "${var.project_name}-private"
   }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_1" {
+  subnet_id      = aws_subnet.private_1.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_2" {
+  subnet_id      = aws_subnet.private_2.id
+  route_table_id = aws_route_table.private.id
 }
 
 resource "aws_internet_gateway" "igw" {
@@ -60,22 +101,4 @@ resource "aws_nat_gateway" "nat" {
 
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
-  }
-
-  tags = {
-    Name = "${var.project_name}-public-rt"
-  }
-}
-
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
-  route_table_id = aws_route_table.public.id
 }
